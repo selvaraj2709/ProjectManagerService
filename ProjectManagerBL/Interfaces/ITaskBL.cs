0832af6ca21407800e39f38be734da92dda38cb3
644082 <644082@PC396013>
2019-01-10 20:23:56 +0530

    Add project files.

---
+++ ProjectManagerBL/Interfaces/ITaskBL.cs
@@ -0,0 +1,17 @@
+﻿using ProjectManagerEntity;
+using System.Collections.Generic;
+
+namespace ProjectManagerBL
+{
+    public interface ITaskBL
+    {
+        List<ParentTaskEntity> GetParentTasks();
+        void AddParentTask(ParentTaskEntity task);
+
+        List<TaskEntity> GetAllTasks(int projectId);
+        TaskEntity GetTaskById(int taskId);
+        void AddTask(TaskEntity task);
+        void UpdateTask(TaskEntity task);
+        void EndTask(int taskId);
+    }
+}
---
+++ ProjectManagerEntity/ParentTaskEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagerEntity
+{
+    public class ParentTaskEntity
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+    }
+}
